6eb504b120f299f3a3562452678587ce38d6252f
TuanNguyen-A <79155070+TuanNguyen-A@users.noreply.github.com>
2022-10-13 22:31:17 +0700

    list, update, delete user API, update order API

--- routes/order.js
+++ routes/order.js
@@ -8,6 +8,7 @@ const OrderController = require('../controllers/order')
 router.route('/add').post(OrderController.add)
 router.route('/list').get(OrderController.index)
 router.route('/delete').post(OrderController.deleteOrder)
+router.route('/update').post(OrderController.updateOrder)
 router.route('/:id').get(OrderController.getOrder)
 
 module.exports = router;
