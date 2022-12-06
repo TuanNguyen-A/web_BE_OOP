80cd78e4d4b5c36cc437a94040cc27299b92eac5
TuanNguyen-A <79155070+TuanNguyen-A@users.noreply.github.com>
2022-12-06 13:41:50 +0700

    connect mongo cloud

--- configs/db.js
+++ configs/db.js
@@ -1,5 +1,6 @@
 module.exports = {
-    url: "mongodb://localhost/",
-    database: "OOP_web",
+    url: "mongodb+srv://tuan303:GDnguyen3032001@cluster0.mxtgx5f.mongodb.net/?retryWrites=true&w=majority",
+    database: "",
     imgBucket: "photos",
 };
+
