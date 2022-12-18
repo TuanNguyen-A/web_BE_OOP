3358ae13f544719642089c36f73d0032fde92983
Tuan303 <tuan.it303@gmail.com>
2022-12-18 20:38:22 +0700

    Home Discount List Api

--- routes/discount.js
+++ routes/discount.js
@@ -5,10 +5,11 @@ const passport = require('passport')
 const { validateBody, validateParam, schemas } = require('../helpers/routerHelpers')
 const DiscountController = require('../controllers/discount')
 
-router.route('/').get( DiscountController.index)
+router.route('/').get(passport.authenticate('jwt', { session: false }), DiscountController.index)
 router.route('/add').post(passport.authenticate('jwt', { session: false }), DiscountController.add)
 router.route('/update').post(passport.authenticate('jwt', { session: false }), DiscountController.updateDiscount)
 router.route('/delete').post(passport.authenticate('jwt', { session: false }), DiscountController.deleteDiscount)
+router.route('/homeDiscountList').get(DiscountController.homeDiscountList)
 router.route('/getDiscountByCode/:code').get(DiscountController.getDiscountByCode)
 router.route('/:id').get(DiscountController.getDiscount)
 
